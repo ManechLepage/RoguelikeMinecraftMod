88eb4f7f3e92fc5744677120bfe68b9f6d8bd89f
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-01-05 18:52:38 -0500

    Added new item

--- src/main/java/net/mediano/roguelikemod/RoguelikeMod.java
+++ src/main/java/net/mediano/roguelikemod/RoguelikeMod.java
@@ -1,8 +1,12 @@
 package net.mediano.roguelikemod;
 
 import com.mojang.logging.LogUtils;
+import net.mediano.roguelikemod.item.ModCreativeModTabs;
+import net.mediano.roguelikemod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,15 +26,31 @@ public class RoguelikeMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
     }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.COIN);
+        }
+    }
+
+
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
---
+++ src/main/java/net/mediano/roguelikemod/item/ModCreativeModTabs.java
@@ -0,0 +1,29 @@
+package net.mediano.roguelikemod.item;
+
+import net.mediano.roguelikemod.RoguelikeMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs
+{
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RoguelikeMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> ROGUELIKE_TAB = CREATIVE_MODE_TABS.register("roguelike_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.COIN.get()))
+                    .title(Component.translatable("creativetab.roguelike_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.COIN.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus)
+    {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
